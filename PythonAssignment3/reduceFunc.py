8e799aec6225770f0d393d6959bf4904e532663d
RaunakJalan <ronakjalan98@gmail.com>
2020-11-21 03:54:15 +0530

    Assignment 3: Code 1.1

---
+++ PythonAssignment3/reduceFunc.py
@@ -0,0 +1,21 @@
+def reduce(func, list):
+	s=0
+	for i in list:
+		s=func(s,i)
+	return s
+
+'''
+example functions for debugging
+'''
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+def mult(a,b):
+	return a*b
+
+
+# syntax: reduce(function name, list)
+print(reduce(sub, [10,20,30,40,50]))
