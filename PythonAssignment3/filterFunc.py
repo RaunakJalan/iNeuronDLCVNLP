10f7fa618a8a8206dfce096a94da0a5ee31689e1
RaunakJalan <ronakjalan98@gmail.com>
2020-11-21 04:09:04 +0530

    Assignment 3: Code 1.2

---
+++ PythonAssignment3/filterFunc.py
@@ -0,0 +1,22 @@
+def filter(func, list):
+	filterArr = [i for i in list if func(i) is True]
+	
+	return filterArr
+
+
+
+'''
+Example Functions
+'''
+def isEven(a):
+	if a%2==0:
+		return True
+	return False
+
+def isOdd(a):
+	if a%2!=0:
+		return True
+	return False
+
+#syntax: filter(function, list)
+print(filter(isEven,[1,2,3,4,5,6,7,8,9,10]))
